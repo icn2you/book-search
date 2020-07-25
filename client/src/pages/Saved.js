692206941188bc42b021aa22c32c38c41ce2abda
Christopher Zenner <christopher.zenner@gmail.com>
2020-07-25 15:52:15 -0700

    feat(pages): add search and saved page shells

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { Main } from '../components'
+import { API } from '../utils'
+
+const useStyles = makeStyles(theme => ({
+  // CSS styles
+}))
+
+const Saved = (props) => {
+  // state variables
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <>
+      // ...
+    </>
+  )
+}
+
+export default Saved
---
+++ client/src/pages/index.js
@@ -0,0 +1,2 @@
+export { default as Saved } from './Saved'
+export { default as Search } from './Search'
