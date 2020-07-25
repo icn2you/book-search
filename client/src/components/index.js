4e4b3efe4f5ec69e5251e7e4ce2123c55ce47b0d
Christopher Zenner <christopher.zenner@gmail.com>
2020-07-25 15:57:04 -0700

    feat(components): add components index

---
+++ client/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Main } from './Main'
+export { default as NavBar } from './NavBar'
+export { default as SearchFrom } from './SearchForm'
+export { default as SearchResult } from './SearchResult'
