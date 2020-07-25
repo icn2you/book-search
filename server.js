75691a47d5e23a294cda04e60d73ebcd84f21863
Christopher Zenner <christopher.zenner@gmail.com>
2020-07-25 14:37:15 -0700

    feat(server): connect to the database

--- server.js
+++ server.js
@@ -6,17 +6,31 @@ const logger = require('morgan')
 
 // HTTP port
 const PORT = process.env.PORT || 3001
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/googlebooksdb'
 
 // Create Express app and march on!
 const app = express()
 
 // Log server activity.
-app.use(logger('dev'));
+app.use(logger('dev'))
 
 // Serve static assets on production server.
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
 }
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+mongoose.connection
+  .on('error', console.error.bind(
+    console, 'An error was encountered connecting to the database.'))
+  .once('connected', () => {
+    console.log('Successfully connected to the database.')
+  })
 
 // Send every request to React app.
 app.get('*', (req, res) => {
