0615124a4e66e6ff868b11ac589e47347dcb35da
Christopher Zenner <christopher.zenner@gmail.com>
2020-07-26 13:06:03 -0700

    feat(components): add header component

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Box, Grid, Typography } from '@material-ui/core'
+import { NavBar } from '../../components'
+
+const Header = () => {
+  return (
+    <header>
+      <NavBar />  
+      <Grid alignItems="center">
+        <Grid item xs={12}>
+          <Box mt={4}>
+            <Typography variant='h2'
+              align="center"
+              gutterBottom>
+              Google Books Search
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </header>   
+  )
+}
+
+export default Header
