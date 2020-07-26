09eb31e7fd3b92412873bebad75bad91b07f3d61
Christopher Zenner <christopher.zenner@gmail.com>
2020-07-26 13:12:48 -0700

    refactor(components): add components to index

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,10 @@
+// primary components
+export { default as Header } from './Header'
 export { default as Main } from './Main'
+export { default as Footer } from './Footer'
+
+// secondary components
+export { default as BookCard } from './BookCard'
 export { default as NavBar } from './NavBar'
-export { default as SearchFrom } from './SearchForm'
+export { default as SearchForm } from './SearchForm'
 export { default as SearchResult } from './SearchResult'
