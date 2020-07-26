8e3f761a4a1c1d5e12535955fa40808e4a0794ab
Christopher Zenner <christopher.zenner@gmail.com>
2020-07-26 13:10:10 -0700

    feat(components): add navbar component

---
+++ client/src/components/NavBar/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { 
+  AppBar, Button, ButtonGroup, Toolbar, Typography 
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  title: {
+    flexGrow: 1,
+    marginLeft: theme.spacing(2)
+  }
+}))
+
+const NavBar = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Google Books
+          </Typography>
+          <ButtonGroup variant="text" 
+            color="primary" 
+            aria-label="app navigation button group">
+            <Button color="inherit" href="/">Search</Button>
+            <Button color="inherit" href="/saved">Saved</Button>
+          </ButtonGroup>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default NavBar
