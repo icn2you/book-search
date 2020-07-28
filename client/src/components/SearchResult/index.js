4828732a87cfc2ca8c5b2a75eb7a98b4c1c7fe05
Christopher Zenner <christopher.zenner@gmail.com>
2020-07-27 22:40:24 -0700

    feat(components): render results with book cards
    - Get search result data from data context.
    - Render search results by passing data as props to book card components.

--- client/src/components/SearchResult/index.js
+++ client/src/components/SearchResult/index.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Box, Paper, Typography } from '@material-ui/core'
+import { DataContext, BookCard } from '../'
 
-const SearchResult = ({ children }) => {
+const SearchResult = () => {
+  const { state } = useContext(DataContext)
+
   return (
     <Paper>
       <Box mt={3} px={2} py={2.5}>
         <Typography variant='h4'
           gutterBottom>
-          Book Results
+          Book Results {
+            state.queryStr ? `for “${state.queryStr}”` : ''
+          }
         </Typography>
-        {children}
+        { state.bookData.length
+          ? state.bookData.map(book =>
+              <BookCard 
+                key={book.id}
+                title={book.volumeInfo.title}
+                isbns={book.volumeInfo.industryIdentifiers}
+                authors={book.volumeInfo.authors}
+                desc={book.volumeInfo.description}
+                link={book.volumeInfo.infoLink}
+                image={book.volumeInfo.imageLinks.thumbnail}
+              />
+            )
+          : <Typography variant="h6" color="secondary">
+              {state.userMsg}
+            </Typography>  
+        }
       </Box>
     </Paper>
   )
