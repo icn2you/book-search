b59b0d64ad4da845754ca6decef798d8c1b2dc31
Christopher Zenner <christopher.zenner@gmail.com>
2020-07-28 04:08:25 -0700

    fix(routes): resolve routing issues
    - Route all requests to app via `router`, rather than `app`.
    - Require `./booksAPI`, rather than `./books`, to accomodate renaming of the file.

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ module.exports = (() => {
   router.use('/api', apiRoutes)
 
   // Send every request to React app.
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'))
+  router.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
   })
 
   return router
