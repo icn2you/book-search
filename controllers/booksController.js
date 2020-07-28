f9622936ec847b0bc960ce4b08e13afa937c3fd9
Christopher Zenner <christopher.zenner@gmail.com>
2020-07-28 03:56:35 -0700

    feat(controllers): add create book operation

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,5 +1,10 @@
 const db = require('../models')
 
-module.exports = (() => {
-  // ...
-})()
+module.exports = {
+  create: (req, res) => {
+    db.Book
+      .create(req.body)
+      .then(docs => res.json(docs))
+      .catch(err => res.status(422).json(err.stack))
+  }
+}
