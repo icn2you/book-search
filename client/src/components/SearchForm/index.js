48f0a8d4b28c966a9875c4a168e7a5c28fc8b8fd
Christopher Zenner <christopher.zenner@gmail.com>
2020-07-27 22:24:48 -0700

    feat(components): add functionality to SearchForm
    - Add event handler for form input changes.
    - Add event handler for form submits, which uses query to invoke API.
    - Update data context with results from query search.

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,27 +1,73 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { 
   Box, Button, FormControl, FormHelperText, Paper, TextField, Typography 
 } from '@material-ui/core'
+import { DataContext } from '../'
+import API from '../../utils/API'
+
+const initFormData = Object.freeze({
+  query: ''
+})
 
 const SearchForm = () => {
+  const [formData, setFormData] = useState(initFormData)
+  const { dispatch } = useContext(DataContext)
+
+  const handleFormInputChg = ev => {
+    const { name, value } = ev.target
+    
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+
+  const handleFormSubmit = ev => {
+    ev.preventDefault()
+
+    console.log(this)
+
+    const queryStr = 
+      formData.query.trim().toLowerCase().replace(/\s+/g, '+')
+
+    // Clear form input.
+    setFormData(initFormData)
+
+    API.queryGoogleBooksAPI(queryStr)
+      .then(data => {
+        // DEBUG:
+        // console.log(data.items)
+        
+        dispatch({ 
+          type: 'update', 
+          data: data.items, 
+          query: formData.query 
+        })
+      })
+      .catch(err => console.error(err.stack))
+  }
+
   return (
     <Paper>
       <Box px={2} py={2.5}>
-        <Typography variant='h3'
+        <Typography variant='h4'
           gutterBottom>
           Book Search
         </Typography>
-        <form noValidate>
+        <form noValidate onSubmit={handleFormSubmit}>
           <FormControl fullWidth>
             <TextField variant="outlined"
               id="search-query"
-              label="Search Term(s)" />
+              name="query"
+              label="Search Term(s)"
+              value={formData.query}
+              onChange={handleFormInputChg} />
             <FormHelperText id="search-query-helper-text">
               Enter the name of a book or author you wish to search for.
             </FormHelperText>
           </FormControl>
           <Box my={1} textAlign="right">
-            <Button>Search</Button>
+            <Button onClick={handleFormSubmit}>Search</Button>
           </Box>
         </form>
       </Box>
