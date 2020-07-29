a8e31ae8904386b40a96ccdf4de56b4c9ae022d0
Christopher Zenner <christopher.zenner@gmail.com>
2020-07-29 00:52:01 -0700

    fix(api): read variables from `.env` file

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,14 @@
-require('dotenv').config();
+require('dotenv').config()
 
 const GOOGLE_BOOKS_API_KEY =
-  process.env.GOOGLE_BOOKS_API_KEY ||
+  process.env.REACT_APP_GOOGLE_BOOKS_API_KEY ||
   'AIzaSyB0A6aFFmA9_wzo-NbaCX-nlQn2ww2svYA'
 
 export default {
   // Google Books API search
   queryGoogleBooksAPI: (searchQuery) => {
-    const queryStr = 
+    const queryStr =
       `${searchQuery}&key=${GOOGLE_BOOKS_API_KEY}`
-
-    // DEBUG:
-    /* console.log(
-      `process.env.GOOGLE_BOOKS_API_KEY: 
-      ${process.env.GOOGLE_BOOKS_API_KEY}`) */
-    // console.log(`query string: ${queryStr}`)
 
     return fetch(
       `https://www.googleapis.com/books/v1/volumes?q=${queryStr}`)
@@ -23,15 +17,15 @@ export default {
   },
 
   getBooks: (filter) => fetch(`/api/books?${filter}`)
-    .then((resp => resp.json()))
-    .catch((err => console.error(err.stack))),
+    .then(resp => resp.json())
+    .catch(err => console.error(err.stack)),
 
   saveBook: (book) => fetch('/api/books', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(book)
   })
-    .then((resp => resp.json()))
+    .then(resp => resp.json())
     .catch(err => console.error(err.stack)),
 
   deleteBook: (id) => fetch(`/api/books/${id}`, {
