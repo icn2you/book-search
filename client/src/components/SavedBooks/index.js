a299d805e7a9f8acff55fc4f05bb4cb55a56dcfd
Christopher Zenner <christopher.zenner@gmail.com>
2020-07-28 20:57:26 -0700

    feat(components): add saved books component

---
+++ client/src/components/SavedBooks/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'
+import { BookCard } from '../'
+
+const SavedBooks = ({ books, msg }) => {
+  return (
+    <>
+      <Typography variant='h4'
+        gutterBottom>
+        Your Saved Books
+      </Typography>
+      { books.length
+        ? books.map(book =>
+            <BookCard 
+              key={`${book._id}`}
+              title={book.title}
+              isbns={book.isbns}
+              authors={book.authors}
+              desc={book.description}
+              link={book.link}
+              image={book.image}
+            />
+          )
+        : <Typography variant="h6" color="secondary">
+            {msg}
+          </Typography>  
+      }
+  </>
+  )
+}
+
+export default SavedBooks
