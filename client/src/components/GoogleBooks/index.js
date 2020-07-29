58a86774af3e36c4c8c21deacb4106a6cb6e9802
Christopher Zenner <christopher.zenner@gmail.com>
2020-07-28 20:56:49 -0700

    feat(components): add google books component

---
+++ client/src/components/GoogleBooks/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'
+import { BookCard } from '../'
+
+const GoogleBooks = ({ search, books, msg }) => {
+  return (
+    <>
+      <Typography variant='h4'
+        gutterBottom>
+        Book Results {
+          search ? `for “${search}”` : ''
+        }
+      </Typography>
+      { books.length
+        ? books.map(book =>
+            <BookCard 
+              key={`${book.id}${book.etag}`}
+              title={book.volumeInfo.title}
+              isbns={book.volumeInfo.industryIdentifiers}
+              authors={book.volumeInfo.authors}
+              desc={book.volumeInfo.description}
+              link={book.volumeInfo.infoLink}
+              image={book.volumeInfo.imageLinks.thumbnail}
+            />
+          )
+        : <Typography variant="h6" color="secondary">
+            {msg}
+          </Typography>  
+      }
+  </>
+  )
+}
+
+export default GoogleBooks
