994487361c4b1781569d6e84abd5f7a9afd02108
Christopher Zenner <christopher.zenner@gmail.com>
2020-12-08 20:05:28 -0800

    refactor(api): remove google books api key
    - Remove Google Books API key. It is not necessary to make basic queries
      to the API for public data. Moreover, it presents a security issue being embedded client-side.
    - Revoke Google Books API key to mitigate further risk of exposure.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,11 @@
 require('dotenv').config()
 
-const GOOGLE_BOOKS_API_KEY =
-  process.env.REACT_APP_GOOGLE_BOOKS_API_KEY ||
-  'AIzaSyB0A6aFFmA9_wzo-NbaCX-nlQn2ww2svYA'
-
 export default {
   // Google Books API search
-  queryGoogleBooksAPI: (searchQuery) => {
-    const queryStr =
-      `${searchQuery}&key=${GOOGLE_BOOKS_API_KEY}`
-
-    return fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=${queryStr}`)
-      .then((resp) => resp.json())
-      .catch(err => console.error(err.stack))
-  },
+  queryGoogleBooksAPI: (searchQuery) => fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${searchQuery}`)
+    .then((resp) => resp.json())
+    .catch(err => console.error(err.stack)),
 
   getBooks: (filter) => fetch(`/api/books?${filter}`)
     .then(resp => resp.json())
